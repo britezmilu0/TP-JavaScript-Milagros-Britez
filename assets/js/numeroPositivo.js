0a8ea275fa1aebdb8606574dcbf5167e159160f1
milagros britez <britezmilu0@gmail.com>
2024-05-03 15:41:12 -0300

    Se realizo la actividad numero positivo/negativo

---
+++ assets/js/numeroPositivo.js
@@ -0,0 +1,9 @@
+let numero= parseInt(prompt( "ingrese un numero entero"))
+console.log(numero)
+
+if (numero %2 === 0)
+    console.log ('El numero ingresado es par')
+else {
+    console.log (' El numero ingresado es negativo')
+}
+    
