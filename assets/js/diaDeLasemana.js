834be0c123c367637d4a3e8ea54df35e29be1c1a
milagros britez <britezmilu0@gmail.com>
2024-05-06 09:57:01 -0300

    se realizo la actividad de los dias de la semanda

---
+++ assets/js/diaDeLasemana.js
@@ -0,0 +1,26 @@
+let dia=parseInt (prompt("ingrese un numero del 1 al 7"));
+
+switch (dia) {
+    case 1: 
+        console.log("Lunes")
+        break;
+    case 2: 
+        console.log( "Martes")
+        break;
+    case 3:
+        console.log("Miercoles")
+        break;
+    case 4:
+        console.log("Jueves")
+        break;
+    case 5:
+        console.log('Viernes')
+        break; 
+    case 6:
+        console.log('Sabado');
+        break;
+    case 8:
+    console.log('Domingo');
+    break; 
+    
+}
